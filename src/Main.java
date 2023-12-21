4ba108c4602ce95da5a9dbd979f5dc422cb9b37a
MohAmin22 <alsayedm12@gmail.com>
2023-12-21 06:35:14 +0200

    added .jar files, report

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        /*
+        I acknowledge that I am aware of the academic integrity guidelines of this course,
+        and that I worked on this assignment independently without any unauthorized help
+         */
         String mode = args[0];
         String inputPath = args[1];
         int numberOfBytesPerWord = args.length == 3 ? Integer.parseInt(args[2]) : 0;
